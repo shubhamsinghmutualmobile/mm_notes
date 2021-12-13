20d493fab3310ff1f75a6cd9e458a6c3be8c494e
Shubham Singh <shubham.singh@mutualmobile.com>
2021-12-13 12:45:01 +0530

    Implemented TrashScreen AppBar (UI only)

--- lib/screens/trash_screen/trash_screen.dart
+++ lib/screens/trash_screen/trash_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class TrashScreen extends StatelessWidget {
   const TrashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Trash Screen!"),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert, color: Get.textTheme.headline4?.color,))
+        ],
+        leading: IconButton(onPressed: () {
+          Scaffold.of(context).openDrawer();
+        }, icon: Icon(Icons.menu, color: Get.textTheme.headline4?.color,)),
+        title: const Text("Trash",),
+        titleTextStyle: Get.textTheme.headline4?.copyWith(fontSize: 18),
+        titleSpacing: 4,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
     );
   }
 }
